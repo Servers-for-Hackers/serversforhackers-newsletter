2ae0087d865d20732d98fa4641647b5b852cbc1e
fideloper <fideloper@gmail.com>
2014-03-15 11:23:09 -0400

    updated publish.sh

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@
 php ./sculpin.phar generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-rsync -vzrS output_prod/ fido@fidbot:/var/www/serversforhackers.com/public
+rsync -vzrS output_prod/ fidbot@fidbot:/var/www/serversforhackers.com/public
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
